c7b3e3b94c996e290fc72e593e0e8fc7be52f3ed
Jha <pratikpro1999@gmail.com>
2022-11-18 18:42:27 +0530

    basic vue app

---
+++ src/main.js
@@ -0,0 +1,10 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import BaseCard from './components/UI/BaseCard.vue';
+import BaseButton from './components/UI/BaseButton.vue'
+import ModalView from './components/UI/ModalView.vue'
+const app = createApp(App)
+app.component('base-card', BaseCard)
+app.component('base-button', BaseButton)
+app.component('modal-view',ModalView)
+app.mount('#app')
